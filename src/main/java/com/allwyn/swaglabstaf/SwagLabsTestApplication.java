9f54bb39fe6275156f953cc8f3e3d7e90cc18972
Roman Kravchenko <kravchenko-rn@ukr.net>
2025-07-04 12:21:47 +0300

    Initial commit

---
+++ src/main/java/com/allwyn/swaglabstaf/SwagLabsTestApplication.java
@@ -0,0 +1,13 @@
+package com.allwyn.swaglabstaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwagLabsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwagLabsTestApplication.class, args);
+	}
+
+}
