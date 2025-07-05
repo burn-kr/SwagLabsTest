3812007ba5a2728de00f3d025b30250a71539605
Roman Kravchenko <kravchenko-rn@ukr.net>
2025-07-05 16:43:30 +0300

    Adjustments

--- src/main/java/com/allwyn/swaglabstaf/ui/component/BaseComponent.java
+++ src/main/java/com/allwyn/swaglabstaf/ui/component/BaseComponent.java
@@ -1,6 +1,7 @@
 package com.allwyn.swaglabstaf.ui.component;
 
 import com.codeborne.selenide.SelenideElement;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
 import static com.codeborne.selenide.Condition.*;
@@ -10,8 +11,9 @@ public abstract class BaseComponent {
 
     protected static final String INPUT_ERROR_MARK_XPATH = "//*[@data-icon = 'times-circle']";
 
-    public SelenideElement element;
-    public String name;
+    protected SelenideElement element;
+    @Getter
+    protected String name;
 
     public String getAlias() {
         return "'%s' %s".formatted(name, this.getClass().getSimpleName().toLowerCase());
--- src/main/java/com/allwyn/swaglabstaf/ui/component/pageControl/Button.java
+++ src/main/java/com/allwyn/swaglabstaf/ui/component/pageControl/Button.java
@@ -1,10 +1,10 @@
 package com.allwyn.swaglabstaf.ui.component.pageControl;
 
 import com.allwyn.swaglabstaf.ui.component.BaseComponent;
+import com.codeborne.selenide.SelenideElement;
 import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 
 @Slf4j
@@ -13,5 +13,10 @@ public class Button extends BaseComponent {
     public Button(String name, By locator) {
         this.name = name;
         this.element = $(locator);
+    }
+
+    public Button(String name, SelenideElement element) {
+        this.name = name;
+        this.element = element;
     }
 }
