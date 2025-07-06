a7c0ea27980cbb5d0fd97b5a67fe1809db8faa74
Roman Kravchenko <kravchenko-rn@ukr.net>
2025-07-06 13:08:26 +0300

    Added inventory test + auth test

--- src/main/java/com/allwyn/swaglabstaf/constant/AssertionMessage.java
+++ src/main/java/com/allwyn/swaglabstaf/constant/AssertionMessage.java
@@ -4,4 +4,8 @@ public class AssertionMessage {
 
     public static final String PAGE_URL_INCORRECT = "Page URL is not as expected";
     public static final String ERROR_MESSAGE_INCORRECT = "The error message is not as expected";
+    public static final String ELEM_LIST_INCORRECT = "The list of elements is not as expected";
+    public static final String BADGE_VALUE_INCORRECT = "The shopping cart badge value is not as expected";
+    public static final String BUTTON_TEXT_INCORRECT = "The button text is not as expected";
+    public static final String PAGE_TITLE_INCORRECT = "The page title is not as expected";
 }
---
+++ src/test/java/com/allwyn/swaglabstaf/ProductsTest.java
@@ -0,0 +1,137 @@
+package com.allwyn.swaglabstaf;
+
+import com.allwyn.swaglabstaf.constant.SortingSelectOption;
+import com.allwyn.swaglabstaf.ui.page.InventoryPage;
+import io.qameta.allure.Description;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import static com.allwyn.swaglabstaf.constant.AssertionMessage.*;
+import static com.allwyn.swaglabstaf.constant.SortingSelectOption.*;
+import static com.allwyn.swaglabstaf.constant.TestGroup.ALL;
+import static com.allwyn.swaglabstaf.constant.TestGroup.PRODUCTS;
+import static com.allwyn.swaglabstaf.constant.UserName.STANDARD_USER;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+
+@Test(groups = {ALL, PRODUCTS}, testName = "Products Test")
+public class ProductsTest extends BaseTest {
+
+    private static final String BACKPACK_PRODUCT_NAME = "Sauce Labs Backpack";
+    private static final String ONESIE_PRODUCT_NAME = "Sauce Labs Onesie";
+    private static final String INVALID_TEXT = "Text contains invalid characters";
+    private static final String EXPECTED_ADD_BUTTON_TEXT = "Add to cart";
+    private static final String EXPECTED_REMOVE_BUTTON_TEXT = "Remove";
+
+    @Autowired
+    private InventoryPage inventoryPage;
+
+    private Pattern forbiddenCombinationPattern;
+
+    @BeforeMethod
+    public void login() {
+        var forbiddenCombination = "^(?!.*\\.[\\s\\S]*?\\(\\)).*$";
+        forbiddenCombinationPattern = Pattern.compile(forbiddenCombination);
+
+        loginPage.login(credentials.getUser(STANDARD_USER));
+    }
+
+    @Test(description = "Product names Test")
+    @Description("Verifies that the product names do not contain any system names/characters")
+    public void productNamesTest() {
+        var productNames = inventoryPage.getProductsNames();
+
+        validateTexts(productNames);
+    }
+
+    @Test(description = "Product descriptions Test")
+    @Description("Verifies that the product descriptions do not contain any system names/characters")
+    public void productDescriptionTest() {
+        var productDescriptions = inventoryPage.getProductsDescriptions();
+
+        validateTexts(productDescriptions);
+    }
+
+    @DataProvider
+    public Object[][] selectOptionProvider() {
+        return new Object[][]  {
+                {A_TO_Z, Comparator.naturalOrder()},
+                {Z_TO_A, Comparator.reverseOrder()},
+                {LOW_TO_HIGH, Comparator.naturalOrder()},
+                {HIGH_TO_LOW, Comparator.reverseOrder()}
+        };
+    }
+
+    @Test(description = "Product sorting Test", dataProvider = "selectOptionProvider")
+    @Description("Verifies that the products get sorted according to the selected option in the sorting select")
+    @SuppressWarnings("unchecked")
+    public <T extends Comparable<T>> void productSortingTest(SortingSelectOption sortingSelectOption, Comparator<T> comparator) {
+        inventoryPage.getHeader().selectSortingOrder(sortingSelectOption);
+
+        var actualProductsOrder = switch (sortingSelectOption) {
+            case A_TO_Z, Z_TO_A -> (List<T>) inventoryPage.getProductsNames();
+            case HIGH_TO_LOW, LOW_TO_HIGH -> (List<T>) inventoryPage.getProductsPrices();
+        };
+
+        var expectedProductsOrder = new ArrayList<>(actualProductsOrder);
+        expectedProductsOrder.sort(comparator);
+
+        assertThat(actualProductsOrder)
+                .as(ELEM_LIST_INCORRECT)
+                .containsExactlyElementsOf(expectedProductsOrder);
+    }
+
+    @Test(description = "Cart badge value Test")
+    @Description("Verifies that the shopping cart badge correctly displays the number of the products in the cart")
+    public void cartBadgeValueTest() {
+        // expected value is 1 as we add 2 items to the shopping cart and then remove 1
+        var expectedBadgeValue = 1;
+        var initialBadgeValue = inventoryPage.getHeader().getBadgeValue();
+
+        var backpackProduct = inventoryPage.getProductByName(BACKPACK_PRODUCT_NAME);
+        var onesieProduct = inventoryPage.getProductByName(ONESIE_PRODUCT_NAME);
+        backpackProduct.clickAddToCartButton();
+        onesieProduct.clickAddToCartButton();
+        backpackProduct.clickRemoveButton();
+
+        var badgeValue = inventoryPage.getHeader().getBadgeValue();
+
+        assertThat(badgeValue - initialBadgeValue)
+                .as(BADGE_VALUE_INCORRECT)
+                .isEqualTo(expectedBadgeValue);
+    }
+
+    @Test(description = "Product button Test")
+    @Description("Verifies that the product button text changes depending on the product status: " +
+            "added/removed to/from the shopping cart")
+    public void productButtonTest() {
+        var backpackProduct = inventoryPage.getProductByName(BACKPACK_PRODUCT_NAME);
+        backpackProduct.clickAddToCartButton();
+        var addedProductButtonText = backpackProduct.getButtonText();
+
+        backpackProduct.clickRemoveButton();
+        var removedProductButtonText = backpackProduct.getButtonText();
+
+        assertSoftly(softAssertions -> {
+            softAssertions.assertThat(addedProductButtonText)
+                    .as(BUTTON_TEXT_INCORRECT)
+                    .isEqualTo(EXPECTED_REMOVE_BUTTON_TEXT);
+            softAssertions.assertThat(removedProductButtonText)
+                    .as(BUTTON_TEXT_INCORRECT)
+                    .isEqualTo(EXPECTED_ADD_BUTTON_TEXT);
+        });
+    }
+
+    private void validateTexts(List<String> textsToValidate) {
+        assertThat(textsToValidate)
+                .as(INVALID_TEXT)
+                .allMatch(name -> forbiddenCombinationPattern.matcher(name).matches());
+    }
+}
